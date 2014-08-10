5a3388066ced6a6fa37188f841cc8fef757d0e6f
Spencer Oberstadt <soberstadt@gmail.com>
2014-08-10 11:32:28 -0500

    implement addAccount

--- src/android/NativeContactsPlugin.java
+++ src/android/NativeContactsPlugin.java
@@ -1,23 +1,28 @@
 package org.mpdx.nativecontacts;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.ContentResolver;
+import android.os.Bundle;
+
 public class NativeContactsPlugin extends CordovaPlugin {
 
+    private static final String ACCOUNT_TYPE = "org.mpdx";
     public static final String ACTION_ADD_ACCOUNT = "addAccount";
     public static final String ACTION_START_SYNC = "startSync";
     public static final String ACTION_REMOVE_ACCOUNT = "removeAccount";
+    private AccountManager mManager;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if(mManager == null)
+            mManager = AccountManager.get(cordova.getActivity());
+
         if(ACTION_ADD_ACCOUNT.equals(action)) {
             String name = args.getString(0);
             String accessToken = args.getString(1);
@@ -37,17 +42,38 @@ public class NativeContactsPlugin extends CordovaPlugin {
 
     private void addAccount(String name, String accessToken, CallbackContext callbackContext) {
         if(name != null && name.length() > 0 && accessToken != null && accessToken.length() > 0) {
-            callbackContext.success();
+            Account account = new Account(name, ACCOUNT_TYPE);
+            if(false == mManager.addAccountExplicitly(account, null, null)) {
+                callbackContext.error("Account with username already exists!");
+            }
+            mManager.setAuthToken(account, ACCOUNT_TYPE, accessToken);
+            callbackContext.success("Account added.");
         } else {
             callbackContext.error("Expected two non-empty string arguments.");
         }
     }
 
     private void removeAccount(CallbackContext callbackContext) {
-        callbackContext.success();
+        callbackContext.success("Not implemented yet.");
     }
 
     private void startSync(CallbackContext callbackContext) {
-        callbackContext.success();
+        Account[] accounts = mManager.getAccountsByType(ACCOUNT_TYPE);
+        if(accounts.length == 0) {
+            callbackContext.error("Expected account to exist.");
+            return;
+        }
+        if(mManager.peekAuthToken(accounts[0], ACCOUNT_TYPE) == null) {
+            callbackContext.error("Expected account to have auth token.");
+            return;
+        }
+        Bundle b = new Bundle();
+        b.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        b.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        ContentResolver.requestSync(
+                accounts[0],
+                ACCOUNT_TYPE,
+                b);
+        callbackContext.success("Sync started!");
     }
 }
